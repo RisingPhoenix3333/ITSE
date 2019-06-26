25ba0f8c2cc7cbdaa7b7e7291c9e4c1284d0db86
RisingPhoenix3333 <wattsty222@gmail.com>
2019-06-25 21:02:13 -0500

    many

--- c#projects/Manymethods/Manymethods/Program.cs
+++ c#projects/Manymethods/Manymethods/Program.cs
@@ -10,29 +10,62 @@ namespace Manymethods
     {
         public static void Main(string[] args)
         {
+
+
+            // calling the method
+            Hello();
+
+            // calling the second method
+            addition();
+
+            // calling the third method
+            Catdog();
+
+            // string interpolation -another way to concatenate strings
+
+            Console.ReadKey(); // exit the console
+        }
+
+        public static void Hello()
+        {
             //print out the message
             Console.WriteLine("Please enter your name.");
 
             // read the input
             var name = Console.ReadLine();
 
-            // print to the console
-            Console.WriteLine("Hello " + name);
-
-            // calling the method
-            Hello(name);
-
-            // string interpolation -another way to concatenate strings
-            Console.WriteLine($"Hello {name}!");
-            Console.ReadKey(); // exit the console
-        }
-
-        public static void Hello(string name)
-        {
             // string concatenation and print to the console
             Console.WriteLine("Hello " + name);
         }
+
+        public static void addition()
+        {
+            Console.WriteLine("Please enter first number");
+
+            int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter second number");
+
+            int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(firstnumber + secondnumber);
+        }
+
+        public static void Catdog()
+        {
+            Console.WriteLine("Do you prefer Cats or Dogs");
+
+            string preference = Console.ReadLine();
+
+            if (preference == "Dogs" || preference == "dogs")
+            {
+                Console.WriteLine("Bark");
+            }
+            if (preference == "Cats" || preference == "cats")
+            {
+                Console.WriteLine("Meow");
+            }
+        }
     }
-   
 }
     
